4440b5e38a521dbcf605df75d1a3a384cbf776f5
Massimiliano Poletto <max.poletto@gmail.com>
2026-03-13 14:51:32 +0100

    Fix test script

--- efaviewer/import.sh
+++ efaviewer/import.sh
@@ -8,9 +8,9 @@ ssh efa@efa.belvoir-rc.ch ls /home/efa/backups | egrep efaBackup.\*zip | tail -1
     xargs -I ff scp efa@efa.belvoir-rc.ch:/home/efa/backups/ff $D/latest.zip && \
 unzip  $D/latest.zip -d $D && \
 python3 bin/efa_importer.py --max-distance 500 \
-    --boats $D/data/BRC/boats.efa2boats \
-    --persons $D/data/BRC/persons.efa2persons \
-    --destinations $D/data/BRC/destinations.efa2destinations \
-    --logbooks $D/data/BRC/20\* \
+    --boats $D/data/BelvoirRC/boats.efa2boats \
+    --persons $D/data/BelvoirRC/persons.efa2persons \
+    --destinations $D/data/BelvoirRC/destinations.efa2destinations \
+    --logbooks $D/data/BelvoirRC/20\* \
     --output app/data/ &&
 gunzip -kf app/data/*.gz
